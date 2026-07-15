62a4dbe196f8cba9643f2fcb3c339235d1f890bd
SebasT807 <sebastian.dev0708@gmail.com>
2026-07-15 18:39:30 -0500

    feat: add Prisma ORM dependencies and export core components for application architecture

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export * from './auth/LoginClient';
+export * from './tecnico/Header';
+export * from './tecnico/RegistrationForm';
+export * from './tecnico/ServiceCard';
+export * from './tecnico/ServicesPanel';
+export * from './ui';

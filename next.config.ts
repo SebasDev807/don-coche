b8e25a49c3a808510fc40d08f45c78a56dd88668
SebasT807 <sebastian.dev0708@gmail.com>
2026-08-08 19:33:06 -0500

    feat: update next.config.ts to allow ngrok tunnel origins for server actions and development environment

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,10 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['pdfkit'],
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000"],
+      allowedOrigins: ["localhost:3000", "*.ngrok-free.app", "*.ngrok.app", "*.ngrok.io"],
     },
   },
+  allowedDevOrigins: ["*.ngrok-free.app", "localhost:3000"],
 };
 
 export default nextConfig;
